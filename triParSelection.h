4ad50ade97e715856b418d71a2ecbc0548f5345e
Pain-da <72081760+Pain-da@users.noreply.github.com>
2020-12-04 15:44:32 +0100

    add chrono

--- triParSelection.h
+++ triParSelection.h
@@ -14,10 +14,12 @@ Compilateur : Mingw-w64 g++ 8.1.0
 #define LABO4_TRIPARSELECTION_H
 #include <vector>
 #include <cstdlib>
+#include <chrono>
 
 
-void triParSelection(std::vector<unsigned>& v, std::vector<unsigned int> &vOperations);
-void triParSelectionMultiVecteur(std::vector<std::vector<unsigned int>> &v, std::vector<unsigned int> &vOperations);
+void triParSelection(std::vector<unsigned>& v, std::vector<unsigned int> &vOperations, std::vector<double> &time);
+void triParSelectionMultiVecteur(std::vector<std::vector<unsigned int>> &v, std::vector<unsigned int> &vOperations,
+                                 std::vector<double> &time);
 
 
 #endif //LABO4_TRIPARSELECTION_H
