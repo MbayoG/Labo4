ad4554048c7708f992ba763be1a9ac95aca68b4b
Pain-da <72081760+Pain-da@users.noreply.github.com>
2020-12-04 10:04:27 +0100

    Add TriParSelection

---
+++ triParSelection.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Stefano on 24.11.2020.
+//
+
+#include "triParSelection.h"
+
+void triParSelectionMultiVecteur(vector<vector<unsigned int>> &v, vector<unsigned int> &vOperations){
+	for(vector<unsigned int> &i : v){
+		triParSelection(i, vOperations);
+	}
+}
+
+void triParSelection(vector<unsigned> &v, vector<unsigned int> &vOperations) {
+
+	unsigned cmp = 0;
+
+	if (v.size() > 0) {
+		size_t iMin;
+
+		for (size_t i = 0; i < v.size() - 1; ++i) {
+			cmp++; // iMin
+			iMin = i;
+
+			for (size_t j = i + 1; j < v.size(); ++j) {
+				cmp++; // v[j] < v[iMin]
+				if (v[j] < v[iMin]) {
+					cmp++; // iMin = j
+					iMin = j;
+				}
+				cmp += 3; // j < v.size() et ++j
+			}
+			cmp += 3; // initialisation de j, i + 1 et premier j < v.size()
+
+			swap(v[i], v[iMin]); // permutation
+			cmp += 3; // ++i, swap et i < v.size()
+		}
+		cmp += 3; // initialisation de i, v.size() - 1 et premier i < v.size()
+	}
+	cmp++; // v.size()
+
+	vOperations.resize(vOperations.size() + 1, cmp);
+}
---
+++ triParSelection.h
@@ -0,0 +1,16 @@
+//
+// Created by Stefano on 24.11.2020.
+//
+
+#ifndef LABO4_TRIPARSELECTION_H
+#define LABO4_TRIPARSELECTION_H
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+
+void triParSelection(vector<unsigned>& v, vector<unsigned int> &vOperations);
+void triParSelectionMultiVecteur(vector<vector<unsigned int>> &v, vector<unsigned int> &vOperations);
+
+
+#endif //LABO4_TRIPARSELECTION_H
