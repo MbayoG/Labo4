6ddf77f4b8e0afc0f0f3907a5c3197465f028023
Pain-da <72081760+Pain-da@users.noreply.github.com>
2020-12-04 15:42:51 +0100

    add chrono

--- triABulle.cpp
+++ triABulle.cpp
@@ -12,16 +12,20 @@ Compilateur : Mingw-w64 g++ 8.1.0
 */
 
 #include "triABulle.h"
+
 using namespace std;
 
-void triABulleMultiVecteur(vector<vector<unsigned int>> &v, vector<unsigned int> &vOperations){
-	for(vector<unsigned int> &i : v){
-		triABulle(i, vOperations);
+void
+triABulleMultiVecteur(vector<vector<unsigned int>> &v, vector<unsigned int> &vOperations, std::vector<double> &time) {
+	for (vector<unsigned int> &i : v) {
+		triABulle(i, vOperations, time);
 	}
 }
 
-void triABulle(vector<unsigned int> &v, vector<unsigned int> &vOperations) {
-int nbOp = 0;
+void triABulle(vector<unsigned int> &v, vector<unsigned int> &vOperations, std::vector<double> &time) {
+	chrono::duration<double> diff;
+	auto start = chrono::high_resolution_clock::now();
+	int nbOp = 0;
 	if (v.size() > 0) {
 		bool fini = false;
 		size_t taille = v.size();
@@ -31,24 +35,20 @@ int nbOp = 0;
 			nbOp += 1;
 			for (size_t i = 0; i < taille - 1; ++i) {
 				if (v[i] > v[i + 1]) {
-					echange(v[i], v[i + 1]);
+					swap(v[i], v[i + 1]);
 					fini = false;
-					nbOp += 7; //nbOperation dans le if et le echange
+					nbOp += 4; //nbOperation dans le if
 				}
-				nbOp +=3; //nbOperation dans le for (sans l'initialisation de i)
+				nbOp += 3; //nbOperation dans le for (sans l'initialisation de i)
 			}
 			--taille;
-			nbOp +=2; // nbOperation taille -- et initialisation de i dans le for
+			nbOp += 2; // nbOperation taille -- et initialisation de i dans le for
 		}
-		nbOp +=1; //nbOperation pour le !fini
+		nbOp += 1; //nbOperation pour le !fini
 	}
-	nbOp +=1; //nbOperation pour le v.size > 0
-	vOperations.resize(vOperations.size() + 1, nbOp);
-}
+	nbOp += 1; //nbOperation pour le v.size > 0
 
-void echange(unsigned int& i, unsigned int& j){
-	unsigned int temp;
-	temp = j;
-	j = i;
-	i = temp;
+	auto end = chrono::high_resolution_clock::now();
+	time.push_back(chrono::duration<double, std::milli>(end - start).count());
+	vOperations.resize(vOperations.size() + 1, nbOp);
 }
