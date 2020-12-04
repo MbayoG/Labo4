fdeaf05bd9e6673d36efb13d81f3f37e3e910518
Pain-da <72081760+Pain-da@users.noreply.github.com>
2020-12-04 15:40:10 +0100

    add affichage vector 10

--- main.cpp
+++ main.cpp
@@ -20,38 +20,95 @@ Compilateur : Mingw-w64 g++ 8.1.0
 
 using namespace std;
 const int NBVECTOR = 27;
+
+void afficheRendu();
+
 int main() {
+	vector<double> timeTriParSelection(0);
+	vector<double> timeTriABulle(0);
+	vector<double> timeTriParInsertion(0);
 	vector<unsigned int> nbOperationTriABulle(0);
 	vector<unsigned int> nbOperationTriParSelection(0);
+	vector<unsigned int> nbOperationTriParInsertion(0);
 	vector<vector<unsigned int>> v10(NBVECTOR);
 	vector<vector<unsigned int>> v100(NBVECTOR);
 	vector<vector<unsigned int>> v1000(NBVECTOR);
 	vector<vector<unsigned int>> v10000(NBVECTOR);
-
-
+	unsigned indiceRandom = 2 + rand() % 25;
 
 	genereVector(10, v10);
 	genereVector(100, v100);
 	genereVector(1000, v1000);
 	genereVector(10000, v10000);
 
+	afficheVector(v10[0]);
+	afficheVector(v10[1]);
+	afficheVector(v10[indiceRandom]);
+	triParInsertionMultiVecteur(v10, nbOperationTriParInsertion, timeTriParInsertion);
+	genereVector(10, v10);
+	triParSelectionMultiVecteur(v10, nbOperationTriParSelection, timeTriParSelection);
+	genereVector(10, v10);
+	triABulleMultiVecteur(v10, nbOperationTriABulle, timeTriABulle);
+	afficheVector(v10[0]);
+	afficheVector(v10[1]);
+	afficheVector(v10[indiceRandom]);
+	cout << "Moyenne du nombre d'operations via le tri a bulle: " << moyenneNbOperation(nbOperationTriABulle) << endl;
+	cout << "Ecart type du nombre d'operations via le tri a bulle: " << ecartTypeOperation(nbOperationTriABulle) << endl;
+	cout << "Moyenne du temps via le tri a bulle: " << moyenneTemps(timeTriABulle) << endl;
 
-	afficheVectorDeVector(v10);
-	//afficheVectorDeVector(v100);
-	//afficheVectorDeVector(v1000);
-	//afficheVectorDeVector(v10000);
-	//triABulleMultiVecteur(v10, nbOperationTriABulle);
-	//triParSelectionMultiVecteur(v10, nbOperationTriParSelection);
-	triParInsertionMultiVecteur(v10, nbOperationTriParSelection);
-	cout << endl << endl;
-	afficheVectorDeVector(v10);
-	//afficheVector(nbOperationTriABulle);
+	cout << "Moyenne du nombre d'operations via le tri par insertion: " << moyenneNbOperation(nbOperationTriParInsertion)
+	     << endl;
+	cout << "Ecart type du nombre d'operations via le tri par insertion: "
+	     << ecartTypeOperation(nbOperationTriParInsertion)
+	     << endl;
+	cout << "Moyenne du temps via le tri par insertion: " << moyenneTemps(timeTriParInsertion) << endl;
+	cout << "Moyenne du nombre d'operations via le tri par selection: " << moyenneNbOperation(nbOperationTriParSelection)
+	     << endl;
+	cout << "Ecart type du nombre d'operations via le tri par selection: " << ecartTypeOperation
+		(nbOperationTriParSelection) << endl;
+	cout << "Moyenne du temps via le tri par selection: " << moyenneTemps(timeTriParSelection) << endl;
+	timeTriParInsertion.clear();
+	timeTriABulle.clear();
+	timeTriParSelection.clear();
+	nbOperationTriABulle.clear();
+	nbOperationTriParInsertion.clear();
+	nbOperationTriParSelection.clear();
 
-	//cout << "Moyenne du nombre d'operations via le tri a bulle: " << moyenneNbOperation(nbOperationTriABulle) << endl;
-	//cout << "Ecart type du nombre d'operations via le tri a bulle: " << ecartTypeOperation(nbOperationTriABulle) << endl;
-	//cout << "Moyenne du nombre d'operations via le tri par selection: " << moyenneNbOperation(nbOperationTriParSelection) << endl;
-	//cout << "Ecart type du nombre d'operations via le tri par selection: " << ecartTypeOperation(nbOperationTriParSelection) << endl;
-	cout << "Moyenne du nombre d'operations via le tri par insertion: " << moyenneNbOperation(nbOperationTriParSelection) << endl;
-	cout << "Ecart type du nombre d'operations via le tri par insertion: " << ecartTypeOperation(nbOperationTriParSelection) << endl;
+
+
+//	afficheVector(v10000[0]);
+//	afficheVector(v10000[1]);
+//	afficheVectorDeVector(v10000);
+//	triParSelectionMultiVecteur(v10000, nbOperationTriParSelection, timeTriParSelection);
+//	genereVector(10000, v10000);
+//	triABulleMultiVecteur(v10000, nbOperationTriABulle, timeTriABulle);
+//	genereVector(10000, v10000);
+//	triParInsertionMultiVecteur(v10000, nbOperationTriParInsertion, timeTriParInsertion);
+//
+//
+//	cout << endl << endl;
+//	afficheVectorDeVector(v10000);
+//
+//
+//	cout << "Moyenne du nombre d'operations via le tri a bulle: " << moyenneNbOperation(nbOperationTriABulle) << endl;
+//	cout << "Ecart type du nombre d'operations via le tri a bulle: " << ecartTypeOperation(nbOperationTriABulle) << endl;
+//	cout << "Moyenne du temps via le tri a bulle: " << moyenneTemps(timeTriABulle) << endl;
+//
+//	cout << "Moyenne du nombre d'operations via le tri par insertion: " << moyenneNbOperation(nbOperationTriParInsertion)
+//	     << endl;
+//	cout << "Ecart type du nombre d'operations via le tri par insertion: "
+//	     << ecartTypeOperation(nbOperationTriParInsertion)
+//	     << endl;
+//	cout << "Moyenne du temps via le tri par insertion: " << moyenneTemps(timeTriParInsertion) << endl;
+//
+//	cout << sommeTemps;
+//	cout << "Moyenne du nombre d'operations via le tri par selection: " << moyenneNbOperation(nbOperationTriParSelection)
+//	     << endl;
+//	cout << "Ecart type du nombre d'operations via le tri par selection: " << ecartTypeOperation
+//		(nbOperationTriParSelection) << endl;
+//	cout << "Moyenne du temps via le tri par selection: " << moyenneTemps(timeTriParSelection) << endl;
+
 	return EXIT_SUCCESS;
 }
+
+
